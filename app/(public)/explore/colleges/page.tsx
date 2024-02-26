43403f790a9366057e34da77248936b6a4cf747d
Ayaansh Rajotia <86037577+ayaanshrajotia@users.noreply.github.com>
2024-02-26 16:46:44 +0530

    minor change: fixed college page bug

--- app/(public)/explore/colleges/page.tsx
+++ app/(public)/explore/colleges/page.tsx
@@ -4,54 +4,7 @@ import React from "react";
 function page() {
     return (
         <div className="mt-4 flex flex-col gap-7">
-            <RoomBoxBigger
-                roomName="Harry Potter"
-                roomId="harrypotter"
-                bgcolor="bg-college-yellow"
-                textColor="black"
-                className="box-shadow-yellow"
-                isPrivate={false}
-            />
-            <RoomBoxBigger
-                roomName="Harry Potter"
-                roomId="harrypotter"
-                bgcolor="bg-college-yellow"
-                textColor="black"
-                className="box-shadow-yellow"
-                isPrivate={false}
-            />
-            <RoomBoxBigger
-                roomName="Harry Potter"
-                roomId="harrypotter"
-                bgcolor="bg-college-yellow"
-                textColor="black"
-                className="box-shadow-yellow"
-                isPrivate={false}
-            />
-            <RoomBoxBigger
-                roomName="Harry Potter"
-                roomId="harrypotter"
-                bgcolor="bg-college-yellow"
-                textColor="black"
-                className="box-shadow-yellow"
-                isPrivate={false}
-            />
-            <RoomBoxBigger
-                roomName="Harry Potter"
-                roomId="harrypotter"
-                bgcolor="bg-college-yellow"
-                textColor="black"
-                className="box-shadow-yellow"
-                isPrivate={false}
-            />
-            <RoomBoxBigger
-                roomName="Harry Potter"
-                roomId="harrypotter"
-                bgcolor="bg-college-yellow"
-                textColor="black"
-                className="box-shadow-yellow"
-                isPrivate={false}
-            />
+         
         </div>
     );
 }
